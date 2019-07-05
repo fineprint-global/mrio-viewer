82a8ddc80704ff3b785d3b31cdf8ab851b31444f
Jakob Gutschlhofer <jakob.gutschlhofer@wu.ac.at>
2019-07-05 16:17:11 +0200

    app: enable bookmarking, add some structuring comments.

--- app/server.R
+++ app/server.R
@@ -1,7 +1,20 @@
 ### server.R
 
+## STRUCTURE
+# 1. Sankey visualization
+# 2. About
+
 # Define server logic
 server <- function(input, output, session) {
+  
+  # Trigger bookmarking with either button
+  # observeEvent(input$bookmark, {
+  #   session$doBookmark()
+  # })
+  
+  ##################################################################
+  ### 1. Sankey visualization
+  ##################################################################
   
   # here, we render the sankey
   output$sankey_plot <- plotly::renderPlotly({
@@ -17,6 +30,19 @@ server <- function(input, output, session) {
                       input$input_year)
       )
   })
+  
+  ### Bookmarking: Read values from state$input when we restore, in order to call them in uiOutput
+  # onRestore(function(state) {
+  #   
+  #   input$input_from_region <- state$input$input_from_region
+  #   input$input_from_product <- state$input$input_from_product
+  #   input$input_year <- state$input$input_year
+  #   
+  # })
+  
+  ##################################################################
+  ### 2. About
+  ##################################################################
   
   output$about_text <- shiny::renderText({
     "<strong>Lorem ipsum</strong> dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."
--- app/ui.R
+++ app/ui.R
@@ -1,66 +1,89 @@
 ### ui.R
 
+## STRUCTURE
+# 1. <HEAD> elements
+# 2. Layout definitions
+# 3. Sankey Visualization
+# 4. About
+
+
 # Define UI
-ui <- tagList(
-  tags$head(
-    tags$link(rel = "stylesheet", type = "text/css", href = "style.css")
-  ),
-  
-  navbarPage(
-    # Application title
-    title = "IO Visualization",
-    selected = "Sankey",
-    # inverse = TRUE,
-    collapsible = TRUE,
-    # theme = "bootstrap.css",
-    theme = shinythemes::shinytheme("flatly"),
-    
-    # Sankey Visualization
-    tabPanel(
-      title = "Sankey",
-      fluidRow(
-        column(
-          width = 2,
-          offset = 2,
-          # here, we put the input elements
-          selectizeInput(inputId = "input_from_region",
-                         label = "From Region",
-                         choices = c("Brazil", "Indonesia"))
-        ),
-        column(
-          width = 2,
-          selectizeInput(inputId = "input_from_product",
-                         label = "From Product",
-                         choices = c("Cattle", "Soy"))
-        ),
-        column(
-          width = 3,
-          sliderInput(inputId = "input_year", 
-                      label = "Year", 
-                      min = 1986,
-                      max = 2013,
-                      value = 2013,
-                      step = 1,
-                      sep = "")
-        )
-      ),
-      fluidRow(
-        plotly::plotlyOutput(outputId = "sankey_plot")
-      )
+ui <- function(request) {
+  tagList(
+    ##################################################################
+    ### 1. <HEAD> elements
+    ##################################################################
+    tags$head(
+      tags$link(rel = "stylesheet", type = "text/css", href = "style.css")
     ),
     
-    # About
-    tabPanel(
-      title = "About",
-      fluidRow(
-        column( width = 12,
-                h1("About"),
-                # we use uiOutput for our renderText because otherwise HTML is not rendered  
-                uiOutput(outputId = "about_text"),
-                h1("Licence"),
-                uiOutput(outputId = "about_licence_text")
+    ##################################################################
+    ### 2. Layout definitions
+    ##################################################################
+    navbarPage(
+      # Application title
+      title = "IO Visualization",
+      selected = "Sankey",
+      # inverse = TRUE,
+      collapsible = TRUE,
+      # theme = "bootstrap.css",
+      theme = shinythemes::shinytheme("flatly"),
+      
+      ##################################################################
+      ### 3. Sankey visualization
+      ##################################################################
+      tabPanel(
+        title = "Sankey",
+        fluidRow(
+          column(
+            width = 2,
+            offset = 2,
+            # here, we put the input elements
+            selectizeInput(inputId = "input_from_region",
+                           label = "From Region",
+                           choices = c("Brazil", "Indonesia"))
+          ),
+          column(
+            width = 2,
+            selectizeInput(inputId = "input_from_product",
+                           label = "From Product",
+                           choices = c("Cattle", "Soy"))
+          ),
+          column(
+            width = 3,
+            sliderInput(inputId = "input_year", 
+                        label = "Year", 
+                        min = 1986,
+                        max = 2013,
+                        value = 2013,
+                        step = 1,
+                        sep = "")
+          ),
+          column(
+            width = 1,
+            bookmarkButton(label = "Share")
+          )
+        ),
+        fluidRow(
+          plotly::plotlyOutput(outputId = "sankey_plot")
+        )
+      ),
+      
+      ##################################################################
+      ### 4. About
+      ##################################################################
+      tabPanel(
+        title = "About",
+        fluidRow(
+          column( width = 12,
+                  h1("About"),
+                  # we use uiOutput for our renderText because otherwise HTML is not rendered  
+                  uiOutput(outputId = "about_text"),
+                  h1("Licence"),
+                  uiOutput(outputId = "about_licence_text")
+          )
         )
       )
     )
   )
-)
+}
